d5cca84b44942202db7ff6fa95400edc2d7d7719
jwanglof <jwanglof@gmail.com>
2013-02-08 16:59:46 +0100

    A LOT OF JUUUUNK IN THE TRUUUUNK

--- js/start.js
+++ js/start.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
+	function hideDiw(except_div) {
+		$("#container div").each(function(i) {
+			var class_name = $(this).attr("class");
+			if (class_name.toLowerCase().indexOf("container") >= 0
+					&& class_name.toLowerCase().indexOf(except_div.toLowerCase())) {
+				$("." + class_name).hide("fast");
+				alert(class_name);
+			}
+		});
+
+		$("." + except_div).show("fast");
+		alert("2" + except_div);
+	}
+
+	// Set the awesome clock in the right corner. WOOOOOP WOOOP
+	var d = new Date();
+	$("#statusbar_clock").text(d.getHours() + ":" + (d.getMinutes()<10?'0':'') + d.getMinutes());
 	setInterval(function() {
 		var d = new Date();
-		//$("#statusbar_clock").text(d.toLocaleTimeString());
 		$("#statusbar_clock").text(d.getHours() + ":" + (d.getMinutes()<10?'0':'') + d.getMinutes());
-	}, 1000);
+	}, 5000);
 
+	hideDiw("start_container");
+	
 	$("a").click(function(event) {
-		var href = $(this).attr("href");
 		event.preventDefault();
-		linkLocation = href;
-		$("body").fadeOut(1, redirectPage);
-		//$("body").slideDown("slow", redirectPage);
+
+		var name = $(this).attr("id");
+		hideDiw(name);
 	});
 
-	function redirectPage() {
-		window.location = linkLocation;
-	}
 });
