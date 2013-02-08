539f4265bd79b7ef0aeaeedc4bb49a613eee2188
andreligne <andre.ligne@gmail.com>
2013-02-08 14:19:16 +0100

    Added some images

--- music/js/musicplayer.js
+++ music/js/musicplayer.js
@@ -4,40 +4,43 @@ $(document).ready(function () {
 
 	$(".album").click(function() {
 		var albuminfo = $("#albuminfo");
+		var object = $(this);
 		var objectIndex = $(this).index() - 1;
 
-		// Just hide if double tap
-		if ($("#last_clicked").text() == objectIndex) {
-			albuminfo.slideUp();
-			$("#last_clicked").text(-1);
-			return;
-		}
-
-		// If even number, set to green, else black
-		if ((objectIndex % 2) == 1) {
-			albuminfo.css("background", "green");
-		}
-		else {
-			albuminfo.css("background", "red");
-		}
-
-		var bodyWidth = $('body').width();
-		var albumWidth = $(this).outerWidth(true);
-		var elementsOnRow = Math.floor(bodyWidth / albumWidth);
-		var row = Math.floor(objectIndex / elementsOnRow) + 1;
-		var index = (row * elementsOnRow) - 1;
-		var appendOn = $('.album').get(index);
-
-		// Move the albuminfo
-		$("#last_clicked").text(objectIndex);
-		albuminfo.insertAfter(appendOn);
-
-		// Set the index in the h1
-		$("#albuminfo").text(objectIndex);
-
-		// Scroll to the elements and view the info div
-		scrollTo($(this));
-		albuminfo.slideDown();
+		// Slide up before doing anything else
+		albuminfo.slideUp(function() {
+			// Just hide if double tap
+			if ($("#last_clicked").text() == objectIndex) {
+				$("#last_clicked").text(-1);
+				return;
+			}
+	
+			// If even number, set to green, else black
+			if ((objectIndex % 2) == 1) {
+				albuminfo.css("background", "green");
+			}
+			else {
+				albuminfo.css("background", "red");
+			}
+	
+			var bodyWidth = $('body').width();
+			var albumWidth = object.outerWidth(true);
+			var elementsOnRow = Math.floor(bodyWidth / albumWidth);
+			var row = Math.floor(objectIndex / elementsOnRow) + 1;
+			var index = (row * elementsOnRow) - 1;
+			var appendOn = $('.album').get(index);
+	
+			// Move the albuminfo
+			$("#last_clicked").text(objectIndex);
+			albuminfo.insertAfter(appendOn);
+	
+			// Set the index in the h1
+			$("#albuminfo").text(objectIndex);
+	
+			// Scroll to the elements and view the info div
+			scrollTo(object);
+			albuminfo.slideDown();
+		});
 	});
 
 });
@@ -45,6 +48,6 @@ $(document).ready(function () {
 
 function scrollTo(element) {
 	$('html, body').animate({
-		scrollTop: element.offset().top - 50
+		scrollTop: element.offset().top - 100
 	}, 1000);
 };
