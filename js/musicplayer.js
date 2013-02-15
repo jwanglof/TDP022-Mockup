a93cb54a729932b9e4d2f7a55cb9e7a6f4592d05
andreligne <andre.ligne@gmail.com>
2013-02-15 11:10:14 +0100

    Added the music player to the mainsite. Code is dafuq

---
+++ js/musicplayer.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+	$("#last_clicked").hide();
+	$("#pause-button").hide();
+	$("#albuminfo").hide();
+	$("#albuminfo").insertAfter("#musiclibrary");
+
+	// When the #albuminfo isn't used we need to move it
+	$(".album").click(function() {
+		var albuminfo = $("#albuminfo");
+		var object = $(this);
+
+		// Slide up before doing anything else
+		albuminfo.slideUp(function() {
+			// Move it away from this div
+			albuminfo.insertAfter("#musiclibrary");
+
+			// Get a new index
+			var objectIndex = object.index() - 1;
+
+			// Just hide if double tap
+			if ($("#last_clicked").text() == objectIndex) {
+				$("#last_clicked").text(-1);
+				return;
+			}
+	
+			// If even number, set to green, else black
+			if ((objectIndex % 2) == 1) {
+				albuminfo.css("background", "green");
+			}
+			else {
+				albuminfo.css("background", "red");
+			}
+	
+			var bodyWidth = $('body').width();
+			var albumWidth = object.outerWidth(true);
+			var elementsOnRow = Math.floor(bodyWidth / albumWidth);
+			var row = Math.floor(objectIndex / elementsOnRow) + 1;
+			var index = (row * elementsOnRow) - 1;
+			var appendOn = $('.album').get(index);
+	
+			// Move the albuminfo
+			$("#last_clicked").text(objectIndex);
+			albuminfo.insertAfter(appendOn);
+	
+			// Set the index in the h1
+			$("#albuminfo").text(objectIndex);
+	
+			// Scroll to the elements and view the info div
+			scrollTo(object);
+			albuminfo.slideDown();
+		});
+	});
+
+	// Toggle play/pause
+	$("#playToggle").click(function() {
+		if ($(this).attr('src') == './img/play_button2.png')
+			$(this).attr('src', './img/pause_button.png');
+		else
+			$(this).attr('src', './img/play_button2.png');
+	});
+
+	// Align everything in the middle on the #playercontrol
+	$("#playercontrol").children().each(function(index, e) {
+		var object = $("#playercontrol").children("div").eq(index);
+		var padding = $("#playercontrol").outerHeight() / 2;
+		padding -= ($(this).outerHeight() / 2);
+
+		console.log(padding);
+		$(this).css('margin-top', padding + 'px');
+	});
+
+});
+
+
+function scrollTo(element) {
+	$('html, body').animate({
+		scrollTop: element.offset().top - 100
+	}, 1000);
+};
--- js/start.js
+++ js/start.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
-	function hideDiw(except_div) {
+	// Hide the none used divs
+	$("#music-library").hide();
+
+	// Switch to the music page
+	$("#music-option").click(function() {
+		console.log("Going to the music library");
+
+		// Move the content of the content-div to outside and hide it
+		$("#container > div").hide(function() {
+			$("#container > div").insertAfter("#container");
+
+			// Move in the music-library to the #container
+			$("#container").prepend("#music-library");
+			$("#music-library").show();
+		});
+	});
+
+	/*function hideDiw(except_div) {
 		$("#container div").each(function(i) {
 			var class_name = $(this).attr("class");
 			if (class_name.toLowerCase().indexOf("container") >= 0
@@ -12,7 +29,7 @@ $(document).ready(function () {
 		// Hide all divs except except_div
 		$("." + except_div).show("fast");
 		//alert("2" + except_div);
-	}
+	}*/
 
 	// Set the awesome clock in the right corner. WOOOOOP WOOOP
 	var d = new Date();
@@ -22,13 +39,13 @@ $(document).ready(function () {
 		$("#statusbar_clock").text(d.getHours() + ":" + (d.getMinutes()<10?'0':'') + d.getMinutes());
 	}, 5000);
 
-	hideDiw("start_container");
+	/*hideDiw("start_container");
 	
 	$("a").click(function(event) {
 		event.preventDefault();
 
 		var name = $(this).attr("id");
 		hideDiw(name);
-	});
+	});*/
 
 });
