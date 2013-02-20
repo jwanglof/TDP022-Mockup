0660dd55c8627a3b493070ae022ca732d39d4000
Anders Ydremark <MrYdremark@gmail.com>
2013-02-20 09:46:21 +0100

    slumpar ETA tid när man ändrar dest

--- js/gps.js
+++ js/gps.js
@@ -1,6 +1,12 @@
 function search()
 {
     var text = document.getElementById("search_box").value;
-    document.getElementById("derp").innerHTML="Destination: " + text;
+    document.getElementById("dest").innerHTML="Destination: " + text;
     document.getElementById("search_box").value = "";
+    
+    var min = Math.floor((Math.random()*12)+1);
+    var sec = Math.floor((Math.random()*60)+1);
+
+    document.getElementById("eta").innerHTML="ETA: " + min + " min and " + sec + " sec";
+
 }
