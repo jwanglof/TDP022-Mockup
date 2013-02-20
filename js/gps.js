59d8d70c6ce2d65b8ba572589d03aab7d4c99838
Anders Ydremark <MrYdremark@gmail.com>
2013-02-20 10:43:55 +0100

    mer

--- js/gps.js
+++ js/gps.js
@@ -7,18 +7,26 @@ function search()
     var min = Math.floor((Math.random()*12)+1);
     var sec = Math.floor((Math.random()*60)+1);
 
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var toaMin = minutes + min;
+    if(toaMin > 59) {
+        houres += 1;
+        toaMin /= 60;
+    }
+    
     if(text.toLowerCase() == "harlem") {
         document.getElementById("eta").innerHTML="ETA: 13 min and 37 sec";
-    } else {
-        document.getElementById("eta").innerHTML="ETA: " + min + " min and " + sec + " sec"; 
-        var date = new Date();
-        var hours = date.getHours();
-        var minutes = date.getMinutes();
-        var toaMin = minutes + min;
+        var toaMin = minutes + 13;
+        var sec = 37;
         if(toaMin > 59) {
             houres += 1;
             toaMin /= 60;
         }
+        document.getElementById("toa").innerHTML="Time on arrival: " + hours + ":" + toaMin + ":" + sec;
+    } else {
+        document.getElementById("eta").innerHTML="ETA: " + min + " min and " + sec + " sec"; 
         document.getElementById("toa").innerHTML="Time on arrival: " + hours + ":" + toaMin + ":" + sec;
     }
 
