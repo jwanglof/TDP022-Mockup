b4c234ac6d6aae66980b572cfaa2cccd57eb294e
Anders Ydremark <MrYdremark@gmail.com>
2013-02-20 10:30:39 +0100

    fixat med ETA och annan data

--- js/gps.js
+++ js/gps.js
@@ -7,6 +7,19 @@ function search()
     var min = Math.floor((Math.random()*12)+1);
     var sec = Math.floor((Math.random()*60)+1);
 
-    document.getElementById("eta").innerHTML="ETA: " + min + " min and " + sec + " sec";
+    if(text.toLowerCase() == "harlem") {
+        document.getElementById("eta").innerHTML="ETA: 13 min and 37 sec";
+    } else {
+        document.getElementById("eta").innerHTML="ETA: " + min + " min and " + sec + " sec"; 
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var toaMin = minutes + min;
+        if(toaMin > 59) {
+            houres += 1;
+            toaMin /= 60;
+        }
+        document.getElementById("toa").innerHTML="Time on arrival: " + hours + ":" + toaMin + ":" + sec;
+    }
 
 }
