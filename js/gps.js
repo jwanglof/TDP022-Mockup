a30611eb29d1f2479350d4b3dee7ec35310a0f9c
Johan Wanglof <jwanglof@gmail.com>
2013-03-13 13:34:12 +0100

    Fixed some formatting on the GPS

--- js/gps.js
+++ js/gps.js
@@ -1,9 +1,8 @@
 function search()
 {	
-
 	var text = document.getElementById("search_box").value;
-	document.getElementById("speed").innerHTML="Speed: 23 km/h";
-	document.getElementById("dest").innerHTML="Destination: " + "<button id=\"newdest\" onclick=\"doSearch()\">" + text + "</button>";
+	document.getElementById("speed").innerHTML="23 km/h";
+	document.getElementById("dest").innerHTML="<button id=\"newdest\" onclick=\"doSearch()\">" + text + "</button>";
 
 	var min = Math.floor((Math.random()*12)+1);
 	var sec = Math.floor((Math.random()*60)+1);
@@ -13,29 +12,29 @@ function search()
 	var minutes = date.getMinutes();
 	var toaMin = minutes + min;
 	if(toaMin > 59) {
-            houres += 1;
-            toaMin /= 60;
+		houres += 1;
+		toaMin /= 60;
 	}
 
 	if(text.toLowerCase() == "bronx") {
-            document.getElementById("speed").innerHTML="Speed: 56 km/h";
+		document.getElementById("speed").innerHTML="56 km/h";
 	} 
 
-        if(text.toLowerCase() == "harlem") {
-            document.getElementById("eta").innerHTML="ETA: 13 min and 37 sec";
-            var toaMin = minutes + 13;
-            var sec = 37; 
-           if(toaMin > 59) {
-		houres += 1;
-		toaMin /= 60;
-           }
-            document.getElementById("toa").innerHTML="Time on arrival: " + hours + ":" + toaMin + ":" + sec;
+	if(text.toLowerCase() == "harlem") {
+		document.getElementById("eta").innerHTML="13 min and 37 sec";
+		var toaMin = minutes + 13;
+		var sec = 37; 
+		if(toaMin > 59) {
+			houres += 1;
+			toaMin /= 60;
+		}
+		document.getElementById("toa").innerHTML=hours + ":" + toaMin + ":" + sec;
 	} else {
-            document.getElementById("eta").innerHTML="ETA: " + min + " min and " + sec + " sec"; 
-            document.getElementById("toa").innerHTML="Time on arrival: " + hours + ":" + toaMin + ":" + sec;
+		document.getElementById("eta").innerHTML= min + " min and " + sec + " sec"; 
+		document.getElementById("toa").innerHTML= hours + ":" + toaMin + ":" + sec;
 	}
 }
 
 function doSearch() {
-    document.getElementById("dest").innerHTML="Destination: " + "<input type=\"text\" id=\"search_box\" value=\"\"/><button id=\"submitbtn\" onclick=\"search()\">GO</button>"
+    document.getElementById("dest").innerHTML= "<input type=\"text\" id=\"search_box\" value=\"\"/><button id=\"submitbtn\" onclick=\"search()\">GO</button>"
 }
